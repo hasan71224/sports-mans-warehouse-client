48473b82d2120e09e3096f67cb889b078ac12d24
Md. Mahmudul Hasan <mdhasan71224@gmail.com>
2022-05-08 03:59:51 +0600

    add my item page delete option

--- src/Pages/MyItem/MyItem.js
+++ src/Pages/MyItem/MyItem.js
@@ -2,29 +2,29 @@ import React from 'react';
 import useItems from '../../hook/useItems';
 
 const MyItem = () => {
-    const [services, setServices] = useItems();
+    const [items, setItems] = useItems();
 
     const handleDelete = id =>{
         const proceed = window.confirm("Are You Sure?");
         if(proceed){
-            const url = `https://radiant-fortress-18374.herokuapp.com/service/${id}`
+            const url = `http://localhost:5000/item/${id}`
             fetch(url, {
                 method: 'DELETE'
             })
             .then(res=>res.json())
             .then(data=>{
                 console.log(data);
-                const remaining = services.filter(service => service._id !== id)
-                setServices(remaining)
+                const remaining = items.filter(item => item._id !== id)
+                setItems(remaining)
             })
         }
     }
     return (
         <div className='w-50 mx-auto'>
-            <h2>Manage your services</h2>
+            <h2>Manage your Sports Item</h2>
             {
-                services.map(service=> <div key={service._id}>
-                    <h6>{service.name} <button onClick={()=>handleDelete(service._id)}>x</button></h6>
+                items.map(item=> <div key={item._id}>
+                    <h6>{item.name} <button onClick={()=>handleDelete(item._id)}>x</button></h6>
                 </div>)
             }
         </div>
